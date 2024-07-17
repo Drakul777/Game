61aa0643a595e227860cd1b80f27da50e4b389a6
Drakul777 <drakul.2011.777@gmail.com>
2024-07-17 20:22:40 +0200

    Add files via upload

--- projectile.py
+++ projectile.py
@@ -13,12 +13,20 @@ class Projectile(pygame.sprite.Sprite):
         self.rect = self.image.get_rect()
         self.rect.x = player.rect.x + 120
         self.rect.y = player.rect.y + 80
+        self.origin_image = self.image
+        self.angle = 0
+
+    def rotate(self):
+        # tourner le projectile
+        self.angle += 8
+        self.image = pygame.transform.rotozoom(self.origin_image, self.angle, scale=1)
+        self.rect = self.image.get_rect(center=self.rect.center)
 
     def remove(self):
         self.player.all_projectiles.remove(self)
     def move(self):
         self.rect.x += self.velocity
-
+        self.rotate()
         #verifier si notre projectile n est plus present dans l ecran
         if self.rect.x > 1080:
             #supprimer le projectile (en dehors de l ecran)
